05adccc020694ac982e1a00e7a84db5ec278c034
malonecab <malonecab@gmail.com>
2011-03-25 20:09:51 +0100

    Diseno nuevo

---
+++ app/controllers/play_controller.rb
@@ -0,0 +1,129 @@
+class PlayController < ApplicationController
+
+ def start
+    #session[:hits_translation] = 0
+    #@translations = []
+    #@hits_translation = session[:hits_translation]
+
+    @hits_article = Get_hits_article()
+    play_list = Get_play_list()
+    @nouns_played = play_list.length
+
+    unless @nouns_played == Noun.count
+      @noun = Noun.find(:random)              #obtenemos la palabra a mostrar
+      #@translations = get_random_translations(@noun)
+      Push_play_list(@noun.id)
+      respond_to do |format|
+        format.html # start.html.erb
+      end
+    else
+      redirect_to :action => 'finish'
+    end
+  end
+
+
+    def validation
+      #validamos resultado
+      article_f = params[:commit]     #articulo seleccionado
+      article = params[:article]
+
+      #translation_f = params[:translation_f]        #traducción seleccionada
+      #translate = params[:translate]
+
+      @hit_article = 0
+      #@hit_translation = 0
+
+      @word_hit = article.to_s + " " + params[:word].to_s
+
+      if article_f.downcase == article.downcase
+        Push_hits_article()
+        @hit_article = 1
+      end
+    end
+
+   def finish
+     #mostrar resumen
+     @hits_article = Get_hits_article()
+     #@hits_translation = session[:hits_translation]
+     @nouns_played = Get_play_list().length
+
+     reset_session
+
+     respond_to do |format|
+       format.html # start.html.erb
+     end
+   end
+
+  #def Get_play_list_length
+  #  play_list = session[:play_list]
+  #  play_list = [] if play_list.nil?
+  #  return play_list.length
+  #end
+
+ def Get_hits_article
+   session[:hits_article].nil? ? 0 : session[:hits_article]
+ end
+
+  def Get_play_list
+   session[:play_list].nil? ? [] : session[:play_list]
+  end
+
+ def Push_play_list(id)
+    session[:play_list] = [] if session[:play_list].nil?
+    session[:play_list] = session[:play_list].push(id)
+ end
+
+ def Push_hits_article
+    session[:hits_article] = 0 if session[:hits_article].nil?
+    session[:hits_article] = session[:hits_article]+1
+ end
+
+
+  def get_random_translations (first_noun)
+    translations = []
+    translation_list = []
+
+      translations.push(first_noun.translation)    #guardamos su traducción y buscamos otras 2 mas
+      translation_list.push(first_noun.id)
+
+      if Noun.count > 1
+          noun_aux = Noun.find(:random, translation_list)   #segunda traducción
+          translations.push(noun_aux.translation)
+          translation_list.push(noun_aux.id)
+      else
+          translations.push("X")
+      end
+
+      if Noun.count > 2
+          noun_aux = Noun.find(:random, translation_list)   #tercera traducción
+          translations.push(noun_aux.translation)
+      else
+           translations.push("Y")
+       end
+
+      return translations.shuffle!                         #revolvemos!
+  end
+end
+
+      #if translation_f == translate
+        #session[:hits_translation] = session[:hits_translation] + 1
+        #@hit_translation = 1
+      #end
+      #@hits_translation = session[:hits_translation]
+
+      #play_list = session[:play_list]
+      #play_list = [] if play_list.nil?
+      #@nouns_played = play_list.length
+
+      #unless play_list.length == Noun.count
+      #  @noun = Noun.find(:random, play_list)
+      #  #@translations = []
+      #  #@translations = get_random_translations(@noun)
+      #  session[:play_list] = play_list.push(@noun.id)
+      #  @data = session[:play_list]
+      #  render 'start'
+      #else
+        #flash[:notice] = "No quedan palabras para jugar"
+      #  redirect_to :action => 'finish'
+      #end
+    #end
